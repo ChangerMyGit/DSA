1be7bdd4d0b657a6c8efd0b95abc9388ca84602b
ChangerMyGit <lanberaaa@163.com>
2014-11-25 16:49:13 +0800

    B-Tree 分裂

--- Btree.c
+++ Btree.c
@@ -1,6 +1,7 @@
 #include "Btree.h"
 #include <stdlib.h>
 #include <memory.h>
+#include <stdio.h>
 
 BTree * initBtree(int order){
 	BTree * tree = (BTree *)malloc(sizeof(BTree));
@@ -17,7 +18,7 @@ BTNode * newBTNode(BTNode * parent , int order ,int keys_Length){
 	node->child = initVector(order * sizeof(BTNode *));
 	// 至多M-1个关键码
 	node->keys = initVector(keys_Length);
-    setAllValue(node->child,NULL,order);
+	setAllValue(node->child,NULL,order);
 	setAllValue(node->keys,NULL,order-1);
 	return node;
 }
@@ -58,13 +59,51 @@ int binSearchInChild(Vector * keys,int lo , int hi,int key){
 }
 
 void solveOverflow(BTNode * btNode,BTree * tree){
-	BTNode * u;
-	int s;
-	if (tree->_order >= btNode->child->size) 
+	BTNode * u , * temp , * p;
+	int s , i;
+	Elem e , shangyi;
+	if(tree->_order > btNode->keys->size) 
 		return; //递归基：当前节点并未上溢
-	//轴点（此时应有_order = key.size() = child.size() - 1）
+	// 轴点（此时应有_order = key.size() = child.size() - 1）
 	s = tree->_order/2;
-	//u = newBTNode(NULL,);
+	// 上溢节点
+	shangyi = getElem(btNode->keys,s);
+	// 创建新的节点
+	u = newBTNode(NULL,s, (s - 1) * sizeof(int));
+	// 分裂孩子关系 从S+1 开始 到 order 结束
+	for(i = s + 1;i <= tree->_order;i++){
+		temp = (BTNode *)getElem(btNode->child,i);
+		// 复制到分裂的节点中去
+		insert(u->child,temp);
+	}
+	// 分裂关键码 从S + 1开始 
+	for(i = s + 1;i < tree->_order;i++){
+	    e = getElem(btNode->keys,i);
+		if(i != s)
+			// 复制到分裂的节点中去
+			insert(u->keys,e);
+	}
+	deleteRange(btNode->keys,s,tree->_order -1);
+	deleteRange(btNode->child,s+1,tree->_order);
+	//printVector(btNode->keys);
+	//printf("\n");
+	//printVector(u->keys);
+	//若u的孩子们非空，则
+	for(i = 0;i<u->child->size;i++){
+		temp = (BTNode *)getElem(u->child,i);
+		if(temp)
+			temp->parent = u; //令它们的父节点统一
+	}
+	p = btNode->parent;
+	if(!p){
+	   p = newBTNode(NULL,tree->_order,(tree->_order - 1) * sizeof(int));
+	   tree->root = p;
+	   insert(p->keys,shangyi);
+	   u->parent = p;
+	   btNode->parent = p;
+	   insert(p->child,btNode);
+	   insert(p->child,u);
+	}
 }
 
 void insertBtree(Elem e , BTree * tree){
@@ -78,6 +117,7 @@ void insertBtree(Elem e , BTree * tree){
 		// 空树首先构造一个父节点
 		tree->root = newBTNode(NULL,tree->_order,(tree->_order - 1) * sizeof(int));
 		insert(tree->root->keys,e);
+		insert(tree->root->child,NULL);
 		insert(tree->root->child,NULL);
 		return;
 	}
--- B树.c
+++ B树.c
@@ -4,11 +4,15 @@
 
 int main(){
 	int i;
-	int a[] = {202,100,60,300};
+	int a[] = {202,100,60,300,50};
 	BTNode * node = NULL;
 	BTree * tree = initBtree(5);
 	for(i = 0;i< sizeof(a)/sizeof(int);i++)
 		insertBtree(a[i],tree);
-	printVector(tree->root->keys);
+	//printVector(tree->root->keys);
+	node = searchBtree(100,tree);
+	if(node){
+		printf("%d \n" , node->keys->elem[0]);
+	}
 	return 0;
 }
